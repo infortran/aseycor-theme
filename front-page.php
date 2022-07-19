25424f0d29411436f9762bda7fd40820deff7703
Freddy Perez <freddy.perez.trabajos@gmail.com>
2022-07-19 19:14:30 -0400

    [add] avancees en home con la logica de wordpress

--- front-page.php
+++ front-page.php
@@ -68,14 +68,31 @@
 		<section class="main-section featured-products swiper">
 			<h2 class="section-title">Propiedades destacadas</h2>
 			<div class="featured-products__card-list swiper-wrapper">
+
+<?php 
+$loop = new WP_Query( ['post_type' => 'propiedades', 'posts_per_page' => 10, 'orderBy' => ['date' => 'ASC']] );
+
+function custom_field($field_name, $post_id = false){
+	$value = get_field($field_name, $post_id);
+	if(is_array($value)){
+		$value = @implode(', ', $value);
+	}
+	return $value;
+}
+
+while($loop->have_posts()):
+	$loop->the_post();
+	echo '<a href='.get_permalink().'></a>';
+?>
 				<div class="swiper-slide">
 					<div class="card featured-products__card">
 						<div class="card__header">
 							<div class="card__image-overlay"></div>
 							<div class="card__image-container">
-								<img class="card__image" src="assets/images/card-example.jpg" alt="" />
+								<img class="card__image" src="/wp-content/themes/aseycor/assets/images/card-example.jpg" alt="" />
 							</div>
-							<div class="card__header-tag">Estado</div>
+							<?php //if(custom_field('propiedad_type') === 'venta') :  ?>
+							<div class="card__header-tag " ><?php the_field('propiedad_type') ?></div>
 							<div class="card__image-description">
 								<i class="card__image-description-icon fa-solid fa-images"></i>
 								<p class="card__image-description-text">20</p>
@@ -83,159 +100,39 @@
 						</div>
 						<div class="card__body">
 							<div class="card__title-container">
-								<h3 class="card__title">Nombre de la propiedad</h3>
+								<h3 class="card__title"><?php the_title() ?></h3>
 							</div>
-							<h4 class="card__price">UF 5000</h4>
-							<div class="card__tag">Mensual</div>
+							<h4 class="card__price">
+							<?php echo the_field('propiedad_uf') ? 'UF '. get_field('propiedad_price_uf') : '$ '. number_format(custom_field('propiedad_price'), 0, '','.') ?>
+							</h4>
+							<div class="card__tag" style="<?php echo custom_field('propiedad_type') === 'venta' ? 'visibility:hidden' : '' ?>">Mensual</div>
 							<div class="card__info-container">
 								<div class="card__info">
 									<i class="card__info-icon fa-solid fa-bed"></i>
-									<p class="card__info-text">1</p>
+									<p class="card__info-text"><?php the_field('num_dormitorios') ?></p>
 								</div>
 
 								<div class="card__info">
 									<i class="card__info-icon fa-solid fa-bath"></i>
-									<p class="card__info-text">1</p>
+									<p class="card__info-text"><?php the_field('num_banos') ?></p>
 								</div>
 
 								<div class="card__info">
 									<i class="card__info-icon fa-solid fa-chart-area"></i>
-									<p class="card__info-text">300m2</p>
+									<p class="card__info-text"><?php the_field('area_propiedad') ?>m2</p>
 								</div>
 							</div>
 						</div>
 						<div class="card__footer">
 							<i class="card__footer-icon fa-solid fa-location-dot"></i>
-							<p class="card__footer-text">Direccion de la propiedad</p>
+							<p class="card__footer-text"><?php the_field('propiedad_location') ?> </p>
 						</div>
 					</div>
 				</div>
-				<div class="swiper-slide">
-					<div class="card featured-products__card">
-						<div class="card__header">
-							<div class="card__image-overlay"></div>
-							<div class="card__image-container">
-								<img class="card__image" src="assets/images/card-example.jpg" alt="" />
-							</div>
-							<div class="card__header-tag">Estado</div>
-							<div class="card__image-description">
-								<i class="card__image-description-icon fa-solid fa-images"></i>
-								<p class="card__image-description-text">20</p>
-							</div>
-						</div>
-						<div class="card__body">
-							<div class="card__title-container">
-								<h3 class="card__title">Nombre de la propiedad</h3>
-							</div>
-							<h4 class="card__price">UF 5000</h4>
-							<div class="card__tag">Mensual</div>
-							<div class="card__info-container">
-								<div class="card__info">
-									<i class="card__info-icon fa-solid fa-bed"></i>
-									<p class="card__info-text">1</p>
-								</div>
+<?php echo '</a>' ?>
+<?php endwhile; ?>
 
-								<div class="card__info">
-									<i class="card__info-icon fa-solid fa-bath"></i>
-									<p class="card__info-text">1</p>
-								</div>
-
-								<div class="card__info">
-									<i class="card__info-icon fa-solid fa-chart-area"></i>
-									<p class="card__info-text">300m2</p>
-								</div>
-							</div>
-						</div>
-						<div class="card__footer">
-							<i class="card__footer-icon fa-solid fa-location-dot"></i>
-							<p class="card__footer-text">Direccion de la propiedad</p>
-						</div>
-					</div>
-				</div>
-				<div class="swiper-slide">
-					<div class="card featured-products__card">
-						<div class="card__header">
-							<div class="card__image-overlay"></div>
-							<div class="card__image-container">
-								<img class="card__image" src="assets/images/card-example.jpg" alt="" />
-							</div>
-							<div class="card__header-tag">Estado</div>
-							<div class="card__image-description">
-								<i class="card__image-description-icon fa-solid fa-images"></i>
-								<p class="card__image-description-text">20</p>
-							</div>
-						</div>
-						<div class="card__body">
-							<div class="card__title-container">
-								<h3 class="card__title">Nombre de la propiedad</h3>
-							</div>
-							<h4 class="card__price">UF 5000</h4>
-							<div class="card__tag">Mensual</div>
-							<div class="card__info-container">
-								<div class="card__info">
-									<i class="card__info-icon fa-solid fa-bed"></i>
-									<p class="card__info-text">1</p>
-								</div>
-
-								<div class="card__info">
-									<i class="card__info-icon fa-solid fa-bath"></i>
-									<p class="card__info-text">1</p>
-								</div>
-
-								<div class="card__info">
-									<i class="card__info-icon fa-solid fa-chart-area"></i>
-									<p class="card__info-text">300m2</p>
-								</div>
-							</div>
-						</div>
-						<div class="card__footer">
-							<i class="card__footer-icon fa-solid fa-location-dot"></i>
-							<p class="card__footer-text">Direccion de la propiedad</p>
-						</div>
-					</div>
-				</div>
-				<div class="swiper-slide">
-					<div class="card featured-products__card">
-						<div class="card__header">
-							<div class="card__image-overlay"></div>
-							<div class="card__image-container">
-								<img class="card__image" src="assets/images/card-example.jpg" alt="" />
-							</div>
-							<div class="card__header-tag">Estado</div>
-							<div class="card__image-description">
-								<i class="card__image-description-icon fa-solid fa-images"></i>
-								<p class="card__image-description-text">20</p>
-							</div>
-						</div>
-						<div class="card__body">
-							<div class="card__title-container">
-								<h3 class="card__title">Nombre de la propiedad</h3>
-							</div>
-							<h4 class="card__price">UF 5000</h4>
-							<div class="card__tag">Mensual</div>
-							<div class="card__info-container">
-								<div class="card__info">
-									<i class="card__info-icon fa-solid fa-bed"></i>
-									<p class="card__info-text">1</p>
-								</div>
-
-								<div class="card__info">
-									<i class="card__info-icon fa-solid fa-bath"></i>
-									<p class="card__info-text">1</p>
-								</div>
-
-								<div class="card__info">
-									<i class="card__info-icon fa-solid fa-chart-area"></i>
-									<p class="card__info-text">300m2</p>
-								</div>
-							</div>
-						</div>
-						<div class="card__footer">
-							<i class="card__footer-icon fa-solid fa-location-dot"></i>
-							<p class="card__footer-text">Direccion de la propiedad</p>
-						</div>
-					</div>
-				</div>
+				
 			</div>
 			<div class="swiper-button swiper-button-prev"></div>
 			<div class="swiper-button swiper-button-next"></div>
@@ -248,6 +145,11 @@
 			<!-- <h2 class="section-title">Propiedades destacadas?</h2> -->
 
 			<div class="product-list">
+<?php  
+$mainLoop = new WP_Query( ['post_type' => 'propiedades', 'posts_per_page' => 20, 'orderBy' => ['date' => 'ASC']] );	
+while($mainLoop->have_posts()) :
+	$mainLoop->the_post();
+?>
 				<div class="card">
 					<div class="card__header">
 						<div class="card__image-overlay"></div>
@@ -262,8 +164,7 @@
 					</div>
 					<div class="card__body">
 						<div class="card__title-container">
-							<h3 class="card__title">Nombre de la propiedad de la lechuga de la ensalada del almuerzo
-								norteño</h3>
+							<h3 class="card__title"><?php the_title() ?></h3>
 						</div>
 						<h4 class="card__price">UF 5000</h4>
 						<div class="card__tag">Mensual</div>
@@ -289,47 +190,8 @@
 						<p class="card__footer-text">Direccion de la propiedad</p>
 					</div>
 				</div>
-				<div class="card">
-					<div class="card__header">
-						<div class="card__image-overlay"></div>
-						<div class="card__image-container">
-							<img class="card__image" src="assets/images/card-example.jpg" alt="" />
-						</div>
-						<div class="card__header-tag">Estado</div>
-						<div class="card__image-description">
-							<i class="card__image-description-icon fa-solid fa-images"></i>
-							<p class="card__image-description-text">20</p>
-						</div>
-					</div>
-					<div class="card__body">
-						<div class="card__title-container">
-							<h3 class="card__title">Nombre de la propiedad de la lechuga de la ensalada del</h3>
-						</div>
-						<h4 class="card__price">UF 5000</h4>
-						<div class="card__tag">Mensual</div>
-						<div class="card__info-container">
-							<div class="card__info">
-								<i class="card__info-icon fa-solid fa-bed"></i>
-								<p class="card__info-text">1</p>
-							</div>
-
-							<div class="card__info">
-								<i class="card__info-icon fa-solid fa-bath"></i>
-								<p class="card__info-text">1</p>
-							</div>
-
-							<div class="card__info">
-								<i class="card__info-icon fa-solid fa-chart-area"></i>
-								<p class="card__info-text">300m2</p>
-							</div>
-						</div>
-					</div>
-					<div class="card__footer">
-						<i class="card__footer-icon fa-solid fa-location-dot"></i>
-						<p class="card__footer-text">Direccion de la propiedad</p>
-					</div>
-				</div>
-				<div class="card">
+<?php endwhile; ?>
+				<!--div class="card">
 					<div class="card__header">
 						<div class="card__image-overlay"></div>
 						<div class="card__image-container">
@@ -729,6 +591,46 @@
 						<p class="card__footer-text">Direccion de la propiedad</p>
 					</div>
 				</div>
+				<div class="card">
+					<div class="card__header">
+						<div class="card__image-overlay"></div>
+						<div class="card__image-container">
+							<img class="card__image" src="assets/images/card-example.jpg" alt="" />
+						</div>
+						<div class="card__header-tag">Estado</div>
+						<div class="card__image-description">
+							<i class="card__image-description-icon fa-solid fa-images"></i>
+							<p class="card__image-description-text">20</p>
+						</div>
+					</div>
+					<div class="card__body">
+						<div class="card__title-container">
+							<h3 class="card__title">Nombre de la propiedad de la lechuga de la ensalada del</h3>
+						</div>
+						<h4 class="card__price">UF 5000</h4>
+						<div class="card__tag">Mensual</div>
+						<div class="card__info-container">
+							<div class="card__info">
+								<i class="card__info-icon fa-solid fa-bed"></i>
+								<p class="card__info-text">1</p>
+							</div>
+
+							<div class="card__info">
+								<i class="card__info-icon fa-solid fa-bath"></i>
+								<p class="card__info-text">1</p>
+							</div>
+
+							<div class="card__info">
+								<i class="card__info-icon fa-solid fa-chart-area"></i>
+								<p class="card__info-text">300m2</p>
+							</div>
+						</div>
+					</div>
+					<div class="card__footer">
+						<i class="card__footer-icon fa-solid fa-location-dot"></i>
+						<p class="card__footer-text">Direccion de la propiedad</p>
+					</div>
+				</div-->
 
 			</div>
 			<div class="product-list__button-container">
@@ -777,7 +679,7 @@
 		<div class="footer__content">
 			<div class="footer__section footer-logo">
 				<div class="footer-logo__image-container">
-					<img class="footer-logo__image" src="assets/images/logo-transparent.png" alt="">
+					<img class="footer-logo__image" src="/wp-content/themes/aseycor/assets/images/logo-transparent.png" alt="">
 				</div>
 				<p class="footer-logo__description">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea illo
 					cumque illum, hic alias laborum esse! Sint voluptatum facere laudantium?</p>
